f17bdbec22f330e20ef1d586a0730d3ca2dc3365
fontes-mrc <62485413+fontes-mrc@users.noreply.github.com>
2023-12-02 14:21:08 -0300

    2022 day1 python

---
+++ 2022/day1/solution.py
@@ -0,0 +1,32 @@
+import pathlib
+
+
+def get_input() -> list:
+    dir_ = pathlib.Path(__file__).parent.absolute()
+    with open(dir_ / "input.txt") as f:
+        return f.readlines()
+
+
+def main() -> None:
+    inputs: list[str] = get_input()
+    elves = []
+    elf = 0
+
+    for input in inputs:
+        number = input.replace("\n", "")
+        if number != "":
+            elf += int(number)
+        else:
+            elves.append(elf)
+            elf = 0
+
+    # part 1
+    print(max(elves))
+    
+    # part 2
+    elves.sort()
+    print(sum(elves[-3:]))
+
+
+if __name__ == "__main__":
+    main()
